35a8c63e3fb5d9784feabee299b606ceff3d661d
JaguarJack <njphper@gmail.com>
2023-02-20 16:36:26 +0800

    chore: 更新文档仓库地址

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarCollapsed: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/catch-admin/new-doc',
 
           includeCurrentVersion: true,
           lastVersion: 'current',
